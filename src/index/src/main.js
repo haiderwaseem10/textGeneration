d631a3e55221e5627b455e87a6f07265f7c37d0a
hira.shehbaz <hamid.ijaz@kwanso.com>
2024-06-27 11:31:13 +0500

    fixes

--- src/index/src/main.js
+++ src/index/src/main.js
@@ -1,33 +1,27 @@
-import { Client } from 'node-appwrite';
+import { HfInference } from '@huggingface/inference';
 
 // This is your Appwrite function
 // It's executed each time we get a request
-export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
-
-  // You can log messages to the console
-  log('Hello, Logs!');
-
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
-
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
+export default async ({ req, res }) => {
+  if (!req.body.prompt || typeof req.body.prompt !== 'string') {
+    return res.json({
+        ok: false,
+        error: 'Missing required field `prompt`'
+    }, 400);
   }
 
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
-};
+
+  const hf = new HfInference(process.env.HUGGINGFACE_ACCESS_TOKEN);
+
+  try {
+      const completion = await hf.textGeneration({
+          model: 'mistralai/Mistral-7B-Instruct-v0.2',
+          inputs: req.body.prompt,
+          max_new_tokens: req.body.max_new_tokens || 200,
+      });
+      return res.json({ ok: true, completion }, 200);
+  } catch (err) {
+      return res.json({ ok: false, error: 'Failed to query model.' }, 500);
+  }
+}
+
